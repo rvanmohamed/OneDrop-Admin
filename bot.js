27c02c50d828819d384eb77bb53a0d8459e7a9fe
rvanmohamed <49373527+rvanmohamed@users.noreply.github.com>
2019-04-08 13:46:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,44 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-
 client.login(process.env.BOT_TOKEN);
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', Hi => {
+  if (msg.content === 'Hello') {
+    msg.reply('Hello!');
+  }
+});
+client.login(process.env.BOT_TOKEN);
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', -help => {
+  if (msg.content === 'mmkn ttfdl el Guorp dh tklm 7d mn el STAFF Atfdl https://www.facebook.com/groups/OneDropCF/') {
+    msg.reply('mmkn ttfdl el Guorp dh tklm 7d mn el STAFF Atfdl https://www.facebook.com/groups/OneDropCF/');
+  }
+});
+client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
+
+
+
